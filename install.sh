9426cd36cbb759a36f1767c07b57d133a26af108
adnan.drina@gmail.com <adnan.drina@gmail.com>
2020-12-22 17:11:38 +0100

    housekeeping

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@
 echo "Creating Sealed Secrets namespace"
 oc apply -f ss-namespace.yaml
 echo "Sealed Secrets namespace created!"
-
+echo " "
 echo "Creating Sealed Secrets OperatorGroup"
 oc apply -f ss-og.yaml
 echo "Sealed Secrets OperatorGroup created!"
-
+echo " "
 echo "Creating Sealed Secrets Subscription"
 oc apply -f ss-sub.yaml
 echo "Sealed Secrets Subscription created!"
